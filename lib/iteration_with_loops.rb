45ddc905397070185a141c634a6ccd14821a74bd
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-25 03:44:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,16 +1,5 @@
 def join_nested_strings(src)
-  phrase = []
-  row_index = 0
-  while row_index < src.count do
-    element_index = 0
-      while element_index < src[row_index].count do
-        if src[row_index].select { |src[row_index][element_index]| src[row_index][element_index].class == String}
-        phrase << src[row_index][element_index]
-      end
-        element_index += 1 
-    end
-    row_index += 1
-  end
-phrase.join(" ")
+ src.flatten
+ return src
   end
    
