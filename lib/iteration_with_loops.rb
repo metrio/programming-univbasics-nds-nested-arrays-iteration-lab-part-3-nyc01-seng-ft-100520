cf3b6f02e211c584a233ce4e7cb9d5f613bcf6ea
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-25 03:55:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,9 @@
 def join_nested_strings(src)
   new_array = []
+  phrase = []
   new_array << src.flatten
   if index.select  { |index| == String }
-    
+    phrase << index
   end
+  return phrase.join
    
